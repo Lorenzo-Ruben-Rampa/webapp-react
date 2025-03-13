ae4b8126dab9dde9cb7f77405570d9aeca55388a
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-03-13 13:04:12 +0100

    install axios e fx fetchMovies

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,20 @@
+import axios from 'axios';
 import MovieCard from './../components/MovieCard';
 
 const Homepage = () => {
+
+    const fetcMovies = () => {
+        axios.get("http://localhost:3000/api/movies")
+            .then(
+                res => {
+                    console.log(res.data);
+                }
+            )
+            .catch(err => console.log(err))
+    }
+
+    fetcMovies();
+
     return (
         <>
             <h1 className="text-primary">Home</h1>
